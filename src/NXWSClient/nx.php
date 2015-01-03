b8d4a3745671e0ad781831da35db866055fb8e7b
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-01-03 14:53:13 -0300

    Added response_code method.

--- src/NXWSClient/nx.php
+++ src/NXWSClient/nx.php
@@ -7,7 +7,9 @@ class nx {
 		 $config,
 		 $endpoint,
 		 $root_folder,
-		 $folders;
+		 $folders,
+		 $log_file,
+		 $merchant_uid;
 
   /**
    * @param String $environment
@@ -22,35 +24,74 @@ class nx {
 
 	$config_file = $root_folder . DIRECTORY_SEPARATOR . "config.ini";
 	if (!file_exists($config_file)) {
-	  exit("O arquivo $config_file nao existe\n");
+	  exit("O arquivo $config_file nao existe." . PHP_EOL);
 	}
 
-	$this->config = $config = parse_ini_file($config_file, true);
+	$this->config = $config = parse_ini_file($config_file, TRUE);
 	$this->endpoint = $uri = $config['endpoints'][$environment];
+	$this->set_folder_locations();
+
+	$current_date = date("Y-m-d");
+	$this->log_file = $log_file = $this->folders['tmp'] . DIRECTORY_SEPARATOR . "logs" . DIRECTORY_SEPARATOR . "$current_date.log";
 
 	if (!isset($config['endpoints'][$environment])) {
-	  exit("O arquivo config.ini nao contem a instrucao:\n[endpoints]\n$environment = URI\n");
+	  $time = date('G:i:s');
+	  $log = "----$time----" . PHP_EOL;
+
+	  $eol = PHP_EOL;
+	  print $print = "O arquivo config.ini nao contem a instrucao:$eol [endpoints] $eol $environment = URI$eol";
+	  $log .= $print;
+
+	  file_put_contents($log_file, $log, FILE_APPEND);
+	  exit();
 	}
 
 	if ($check_endpoint) {
 	  $request = Request::get($uri)
 		->send();
-  
-	  $code = $request->code;
-	  if ($code != 200) {
-		exit("Endpoint $uri NAO esta acessivel. Retornou o codigo de erro $code.\n");
-	  }
-	  exit("Endpoint $uri esta acessivel.\n");
+
+	  $this->response_code($request, $uri);
+	  exit("Endpoint $uri esta acessivel." . PHP_EOL);
 	}
 
-	$this->set_folder_locations();
 	$this->login();
   }
 
+  private function response_code($response, $uri) {
+	$code = $response->code;
+
+	if ($code == 200) {
+	  print "SUCESSO!!!" . PHP_EOL;
+	}
+	else {
+	  $body = $response->raw_body;
+	  $time = date('G:i:s');
+	  $log = "----$time----" . PHP_EOL;
+	  $log_file = $this->log_file;
+
+	  print $print = "A chamada ao Endpoint $uri FALHOU. Retornou o Codigo de Status HTTP $code." . PHP_EOL;
+	  $log .= $print;
+	  if (!empty($body)) {
+		print $print = "O Webservice respondeu o seguinte:" . PHP_EOL;
+		$log .= $print;
+		print $print = $body . PHP_EOL;
+		$log .= $print;
+
+		file_put_contents($log_file, $log, FILE_APPEND);
+		exit();
+	  }
+	  file_put_contents($log_file, $log, FILE_APPEND);
+	  exit();
+	}
+  }
+
   private function set_folder_locations() {
-	foreach($this->config['pastas'] as $folder_user => $folder_path) {
-	  $path = str_replace('%app%', $this->root_folder, $folder_path);
-	  $this->folders[$folder_user] = str_replace('/', DIRECTORY_SEPARATOR, $path);
+	foreach($this->config['pastas'] as $folder => $folder_path) {
+	  // Make sure the correct OS directory separator is gonna be in place.
+	  $folder_path = str_replace('/', DIRECTORY_SEPARATOR, $folder_path);
+
+	  // Set this application root path as default root path for this folder.
+	  $this->folders[$folder] = str_replace('%app%', $this->root_folder, $folder_path);
 	}
   }
 
@@ -76,8 +117,13 @@ class nx {
 		->expectsJson()
 		->send();
 
+	  $this->response_code($request, $uri);
+
 	  $sessid = $request->body->sessid;
 	  $session_name = $request->body->session_name;
+
+	  // @TODO: Get merchant uid.
+	  $merchant_uid = $this->merchant_uid = '';
 
 	  $this->session = "$session_name=$sessid";
 	  $this->token = $request->body->token;
@@ -98,15 +144,17 @@ class nx {
 	  ->addHeader('X-CSRF-Token', $this->token)
 	  ->body($body)
 	  ->send();
-	print_r($request->body);
+
+	$this->response_code($request, $uri);
+	return $request->body;
   }
 
   public function create($service = 'produto') {
 	$body = array(
-	  'nome' => 'my new product 2',
-	  'preco' => '5068',
-	  'preco_velho' => '5068',
-	  'qtde_em_estoque' => '99885.00',
+	  'nome' => 'my new product 5',
+	  'preco' => 5068,
+	  'preco_velho' => 7068,
+	  'qtde_em_estoque' => 99885.00,
 	  'cod_cidade' => 35,
 	  // Opcional.
 	  'localizacao_fisica' => 'prateleira',
@@ -114,7 +162,7 @@ class nx {
 	  'cod_produto_erp' => '123',
 	);
 
-	$this->request($service, $body, 'post');
+	$response = $this->request($service, $body, 'post');
   }
 
   public function update() {
