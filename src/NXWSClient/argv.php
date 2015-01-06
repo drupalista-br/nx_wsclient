ea40d7f8949ccd52d7aa1aa9fffc06b2f4ad2ccf
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-01-06 20:07:24 -0300

    ...

--- src/NXWSClient/argv.php
+++ src/NXWSClient/argv.php
@@ -24,17 +24,36 @@ class argv {
 	),
 	'consultar' => array(
 	  'produto' => array(
-		'product_id' => 'Exemplo: php run.php consultar produto product_id VALOR_DO_PRODUCT_ID DESTINO',
-		'sku' => 'Exemplo: php run.php consultar produto sku VALOR_DO_SKU DESTINO',
-		'cod_produto_erp' => 'Exemplo: php run.php consultar produto cod_produto_erp VALOR_DO_COD_PRODUTO_ERP DESTINO',
+		'product_id' => 'Exemplo: php run.php consultar produto product_id VALOR_DO_PRODUCT_ID DESTINO FORMATO_DE_DADOS',
+		'sku' => 'Exemplo: php run.php consultar produto sku VALOR_DO_SKU DESTINO FORMATO_DE_DADOS',
+		'cod_produto_erp' => 'Exemplo: php run.php consultar produto cod_produto_erp VALOR_DO_COD_PRODUTO_ERP DESTINO FORMATO_DE_DADOS',
 	  ),
 	  'pedido' => array(
-		'no' => 'Exemplo: php run.php consultar pedido no NUMERO_DO_PEDIDO DESTINO',
-		'entre' => 'Exemplo: php run.php consultar pedido entre TIMESTAMP_INICIO TIMESTAMP_FIM DESTINO',
+		'no' => 'Exemplo: php run.php consultar pedido no NUMERO_DO_PEDIDO DESTINO FORMATO_DE_DADOS',
+		'entre' => 'Exemplo: php run.php consultar pedido entre TIMESTAMP_INICIO TIMESTAMP_FIM DESTINO FORMATO_DE_DADOS',
 	  ),
 	  'cidades' => 'Exemplo: php run.php consultar cidades DESTINO',
 	),
 	'testar' => 'Exemplo: php run.php testar | Verifica se o Webservice está acessível.',
+	// Aliases.
+	'a' => array(
+	  // Create/Update products.
+	  'p' => 'Exemplo: php run.php a p | Executará: php run.php scaniar produto',
+
+	  // Retrieve a product by product_id.
+	  'cpit' => 'Exemplo: php run.php a cpit VALOR_DO_PRODUCT_ID | Executará: php run.php consultar produto product_id VALOR_DO_PRODUCT_ID tela',
+	  'cpiat' => 'Exemplo: php run.php a cpiat VALOR_DO_PRODUCT_ID | Executará: php run.php consultar produto product_id VALOR_DO_PRODUCT_ID arquivo txt',
+	  'cpiaj' => 'Exemplo: php run.php a cpiaj VALOR_DO_PRODUCT_ID | Executará: php run.php consultar produto product_id VALOR_DO_PRODUCT_ID arquivo json',
+	  // Retrieve a product by SKU.
+	  'cpst' => 'Exemplo: php run.php a cpst VALOR_DO_SKU | Executará: php run.php consultar produto sku VALOR_DO_SKU tela',
+	  'cpsat' => 'Exemplo: php run.php a cpsat VALOR_DO_SKU | Executará: php run.php consultar produto sku VALOR_DO_SKU arquivo txt',
+	  'cpsaj' => 'Exemplo: php run.php a cpsaj VALOR_DO_SKU | Executará: php run.php consultar produto sku VALOR_DO_SKU arquivo json',
+	  // Retrieve a product by cod_produto_erp.
+	  'cpct' => 'Exemplo: php run.php a cpst VALOR_DO_COD_PRODUTO_ERP | Executará: php run.php consultar produto cod_produto_erp VALOR_DO_COD_PRODUTO_ERP tela',
+	  'cpcat' => 'Exemplo: php run.php a cpsat VALOR_DO_COD_PRODUTO_ERP | Executará: php run.php consultar produto cod_produto_erp VALOR_DO_COD_PRODUTO_ERP arquivo txt',
+	  'cpcaj' => 'Exemplo: php run.php a cpsaj VALOR_DO_COD_PRODUTO_ERP | Executará: php run.php consultar produto cod_produto_erp VALOR_DO_COD_PRODUTO_ERP arquivo json',
+
+	),
   ),
   $is_dev,
   // nx object.
@@ -53,10 +72,10 @@ class argv {
   $command_params_qty_sent;
   
   // Output content formats.
-  const JSON = 1;
-  const TXT = 2;
+  const JSON = 'json';
+  const TXT = 'txt';
   // Output destinations.
-  const ONSCREEN = 1;
+  const ONSCREEN = 'tela';
   const FILE = 'arquivo';
 
   public function __construct($is_dev = FALSE) {
@@ -78,6 +97,7 @@ class argv {
 			  case 'help':
 			  case 'config':
 			  case 'consultar':
+			  case 'a':
 				$command_type = $argument_value;
 			  break;
 			}
@@ -125,101 +145,160 @@ class argv {
 	$command_array = $this->command_array;
 
 	switch($command_type) {
+	  case 'a':
+		// Aliases.
+		$this->run_a();
+	  break;
 	  case 'help':
-		// Validation.
-		$this->check_command_params();
-		$param_value_sent = $this->command_params[0];
-
-		$help = $this->expected_arguments;
-
-		print "COMANDO(S) PARA $param_value_sent EH/SAO:";
-		print_r($help[$param_value_sent]);
-		exit(PHP_EOL);
-
+		$this->run_help();
+	  break;
 	  case 'config':
-		// Validation.
-		$this->check_command_params();
-		// Set param value.
-		$param_value_sent = $this->command_params[0];
-
-		// Load config.
-		$root_folder = pathinfo(__DIR__);
-		$root_folder = $this->root_folder = dirname($root_folder['dirname']);
-		$config_file = $root_folder . DIRECTORY_SEPARATOR . "config.ini";
-		$config = parse_ini_file($config_file, TRUE);
-
-		if ($param_value_sent == 'mostrar') {
-		  print "O(S) VALOR(ES) PARA $param_value_sent EH/SAO:";
-		  print_r($config[$param_value_sent]);
-		  exit(PHP_EOL);
-		}
-		else {
-		  // Modify config.
-		  // The value of $depth_pointer will end up being something
-		  // like: [credenciais][username]
-		  $depth_pointer = '';
-		  foreach ($command_array as $param) {
-			$depth_pointer .= "['$param']";
-		  }
-		  eval("\$config" . $depth_pointer . " = \$param_value_sent;");
-
-		  // Write back into config.ini.
-		  $writer = new Ini();
-		  $writer->toFile($config_file, $config);
-		  exit("Nova configuracao salva com sucesso." . PHP_EOL);
-		}
-
+		$this->run_config();
+	  break;
 	  case 'nx':
 	  case 'consultar':
 		$this->nx = new nx($this->is_dev);
-		switch($this->command_string) {
-		  case 'scaniar produto':
-			$this->check_command_params(0);
-		
-		  break;
-		  case 'consultar produto product_id':
-			$this->check_command_params(1);
-		
-		  break;
-		  case 'consultar produto sku':
-			$this->check_command_params(1);
-		
-		  break;
-		  case 'consultar produto cod_produto_erp':
-			$this->check_command_params(1);
-		
-		  break;
-		  case 'consultar pedido no':
-			$this->check_command_params(1);
-			
-		  break;
-		  case 'consultar pedido entre':
-			$this->check_command_params(2);
-			
-		  break;
-		  case 'consultar cidades':
-			$location = $this->check_command_params(0);
-			$content = $this->nx->get_cities();
-			$this->output($content, $location);
-		  break;
-		  case 'testar':
-			$this->check_command_params(0);
-			$this->nx->check_endpoint();
-		  break;
-		}
+		$this->run_nx();
+	  break;
 	}
-
-
   }
 
+  /**
+   * Converts command aliases into full command instructions.
+   */
+  private function run_a() {
+	$this->run_help();
+	$this->run_config();
+	$this->run_nx();
+  }
+
+  /**
+   * Shows command line help.
+   */
+  private function run_help() {
+	// Validation.
+	$this->check_command_params();
+	$param_value_sent = $this->command_params[0];
+
+	$help = $this->expected_arguments;
+
+	print "COMANDO(S) PARA $param_value_sent EH/SAO:";
+	print_r($help[$param_value_sent]);
+	exit(PHP_EOL);
+  }
+
+  /**
+   * Reads and Updates config.ini.
+   */
+  private function run_config() {
+	// Validation.
+	$this->check_command_params();
+	// Set param value.
+	$param_value_sent = $this->command_params[0];
+
+	if ($command_array[0] == 'pastas') {
+	  // Check if folder exists. If not, try to create it.
+	  if (!is_dir($param_value_sent)) {
+		$mkdir = mkdir($param_value_sent, 0777, TRUE);
+		if (!$mkdir) {
+		  exit("O caminho $param_value_sent nao existe e nao foi possivel cria-lo. Verifique se o usuario rodando esta aplicacao tem permissao para criar pastas." . PHP_EOL);
+		}
+	  }
+	}
+
+	// Load config.ini.
+	$root_folder = pathinfo(__DIR__);
+	$root_folder = $this->root_folder = dirname($root_folder['dirname']);
+	$config_file = $root_folder . DIRECTORY_SEPARATOR . "config.ini";
+	$config = parse_ini_file($config_file, TRUE);
+
+	if ($param_value_sent == 'mostrar') {
+	  print "O(S) VALOR(ES) PARA $param_value_sent EH/SAO:";
+	  print_r($config[$param_value_sent]);
+	  exit(PHP_EOL);
+	}
+	else {
+	  // Modify config.
+	  // The value of $depth_pointer will end up being something
+	  // like: [credenciais][username]
+	  $depth_pointer = '';
+	  foreach ($command_array as $param) {
+		$depth_pointer .= "['$param']";
+	  }
+	  eval("\$config" . $depth_pointer . " = \$param_value_sent;");
+
+	  // Write back into config.ini.
+	  $writer = new Ini();
+	  $writer->toFile($config_file, $config);
+	  exit("Nova configuracao salva com sucesso." . PHP_EOL);
+	}
+  }
+
+  /**
+   * Creates, Updates and Retrieves service's items.
+   */
+  private function run_nx() {
+	switch($this->command_string) {
+	  case 'scaniar produto':
+		$this->check_command_params(0);
+
+	  break;
+	  case 'consultar produto product_id':
+		$this->check_command_params(1);
+
+	  break;
+	  case 'consultar produto sku':
+		$this->check_command_params(1);
+
+	  break;
+	  case 'consultar produto cod_produto_erp':
+		$this->check_command_params(1);
+
+	  break;
+	  case 'consultar pedido no':
+		$this->check_command_params(1);
+
+	  break;
+	  case 'consultar pedido entre':
+		$this->check_command_params(2);
+
+	  break;
+	  case 'consultar cidades':
+		$location = $this->check_command_params(0);
+		$content = $this->nx->get_cities();
+		$this->output($content, $location);
+	  break;
+	  case 'testar':
+		$this->check_command_params(0);
+		$this->nx->check_endpoint();
+	  break;
+	}
+  }
+
+  /**
+   * Performs validation on passed paramenters.
+   *
+   * @param Integer $command_params_qty_expected
+   *   Number of parameter values the user is expected to give.
+   */
   private function check_command_params($command_params_qty_expected = 1) {
 	$command_type = $this->command_type;
 	$command_params = $this->command_params;
 	$command_params_qty_sent = $this->command_params_qty_sent;
-	
+
   }
 
+  /**
+   * Outputs the content resulted from a service retrieve.
+   *
+   * @param String $content
+   *   The service retrieve result.
+   *
+   * @param String $location
+   *   Possible values are either 'tela' for printing out the content onscreen
+   *   or 'arquivo' for saving the content into .../dados/consulta/filename.ext
+   */
   private function output($content, $location, $format = self::TXT) {
-	
+
   }
 }
