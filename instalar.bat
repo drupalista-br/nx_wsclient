b02e7c3546947932f46756e5dc7723c382ae382c
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-01-24 09:25:28 -0300

    Added README.md

--- instalar.bat
+++ instalar.bat
@@ -1,12 +1,18 @@
-if "%~1"=="" (
-  set phplocation=c:\php5
-) else (
-  set phplocation=%1
-)
-cd %phplocation%
-ren php.ini-production php.ini
+@echo off
+set phplocation=%cd:~0,2%\php5
+set search=%PATH%
 
-SETX /M PATH "%PATH%;%phplocation%"
-SETX /M PATHEXT "%PATHEXT%;.php"
-assoc .php=phpfile
-ftype phpfile="%phplocation%\php.exe" -f "%1" -- %~2
+if NOT "%search%"=="%search%:%phplocation%=%" (
+    SETX /M PATH "%PATH%;%phplocation%"
+    SETX /M PATHEXT "%PATHEXT%;.PHP"
+    assoc .php=phpfile
+    ftype phpfile="%phplocation%\php.exe" -f "%1" -- %~2
+
+    cd %phplocation%
+    ren php.ini-production php.ini
+    php -r "readfile('https://getcomposer.org/installer');" | php
+    echo @php "%~dp0composer.phar" %*>composer.bat
+)
+php -v
+composer -v
+echo Instalacao concluida.
